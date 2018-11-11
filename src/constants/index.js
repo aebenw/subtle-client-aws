919914c46f6ab99e0a6b41f3d4919f339f1b05e3
github username <github email address>
2018-11-10 21:41:38 -0500

    able to upload files to active storage

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 
 
 export const token = localStorage.getItem("jwt");
-export const URL = "http://localhost:3000/api/v1/"
+// export const URL = "http://localhost:3000/api/v1/"
+export const URL = "https://6787fe6a.ngrok.io/api/v1/"
 export const HEADERS = {"Content-Type": "application/json",
 Accept: "application/json"}
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -4,6 +4,8 @@ import { withRouter, Link } from 'react-router-dom'
 import { token } from '../constants'
 import { setHistory } from '../store/actions/users'
 import ChannelContainer from './ChannelContainer'
+import ActiveStorageProvider from 'react-activestorage-provider'
+import Dropzone from "react-dropzone";
 
 
 class Profile extends Component {
@@ -21,9 +23,57 @@ class Profile extends Component {
 
   render(){
     const { currentUser } = this.props
+    // host: 'c3cb7511.ngrok.io',
 
     return (
       <Fragment>
+
+            <ActiveStorageProvider
+          endpoint={{
+            path: '/users',
+            model: 'User',
+            host: "6787fe6a.ngrok.io",
+            attribute: 'profile',
+            method: 'PUT',
+          }}
+          // headers={ {responseHeader: ['Content-Type', 'Content-Md5' ]}}
+          // directUploadsPath={"/rail/active_storage/direct_uploads"}
+          onSubmit={user => {
+            debugger
+            this.setState({ profile: user.profile })}}
+          render={({ handleUpload, uploads, ready }) => (
+            <div>
+              <input
+                type="file"
+                disabled={!ready}
+                onChange={e => {
+
+                  return handleUpload(e.currentTarget.files)}}
+              />
+
+              {uploads.map(upload => {
+                switch (upload.state) {
+                  case 'waiting':
+                    return <p key={upload.id}>Waiting to upload {upload.file.name}</p>
+                  case 'uploading':
+                    return (
+                      <p key={upload.id}>
+                        Uploading {upload.file.name}: {upload.progress}%
+                      </p>
+                    )
+                  case 'error':
+                    return (
+                      <p key={upload.id}>
+                        Error uploading {upload.file.name}: {upload.error}
+                      </p>
+                    )
+                  case 'finished':
+                    return <p key={upload.id}>Finished uploading {upload.file.name}</p>
+                }
+              })}
+            </div>
+          )}
+        />
         {this.props.currentUser.name ?
           <Fragment>
           <h1>{currentUser.name}'s Profile</h1>
@@ -37,6 +87,10 @@ class Profile extends Component {
       :
       <center><div style={{"margin-top": "10em"}} class="spinner tertiary"></div></center>
     }
+
+
+
+
 
     </Fragment>
     )
