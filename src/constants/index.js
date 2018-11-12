58ae3e967a19e7d850c08984ee232c1b71f82a45
github username <github email address>
2018-11-11 22:07:05 -0500

    working on AWS

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
 
 export const token = localStorage.getItem("jwt");
 // export const URL = "http://localhost:3000/api/v1/"
-export const URL = "https://5e9c4f4a.ngrok.io/api/v1/"
-export const MINURL = "https://5e9c4f4a.ngrok.io"
+export const URL = "https://0bdc3623.ngrok.io/api/v1/"
+export const MINURL = "https://0bdc3623.ngrok.io"
 export const HEADERS = {"Content-Type": "application/json",
 Accept: "application/json"}
